18f90eddad4ea2fa1be07e6774fdcff3dc082843
Emilis Maktuėnas <emilis.em4@gmail.com>
2020-05-10 21:07:35 +0300

    Add about icon use notice

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,5 +1,11 @@
 import React, { ReactElement } from 'react';
 import P from 'Atoms/Text/P';
+import A from 'Atoms/Link/A';
+import styled from 'styled-components/macro';
+
+const AStyled = styled(A)`
+  font-size: inherit;
+`;
 
 const About = (): ReactElement => (
   <P fontSize="medium" fontWeight="light">
@@ -10,6 +16,10 @@ const About = (): ReactElement => (
     <br />
     <br />
     Platform is for registered users only, use navigation to take yourself to login page.
+    <br />
+    <br />
+    Icons made by <AStyled href="https://www.flaticon.com/authors/freepik">Freepik</AStyled> from{' '}
+    <AStyled href="https://www.flaticon.com/">www.flaticon.com</AStyled>
   </P>
 );
 
